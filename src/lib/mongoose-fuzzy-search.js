4bd716a53c25e97e2d1fa173c0fdb966d9c3d104
Tho <myworld20011999@gmail.com>
2022-06-13 13:03:16 +0700

    Update Fuzzy Search - add lodash package

--- src/lib/mongoose-fuzzy-search.js
+++ src/lib/mongoose-fuzzy-search.js
@@ -1,11 +1,16 @@
 // Author Tho Tran
 
+const { isEmpty } = require("lodash");
+
 module.exports = function search(context) {
   const param = context.params;
+  console.log(param.query.$search);
   if (param.query.$search) {
     const query = {};
     Object.keys(param.query.$search).map((item) => {
-      query[item] = { $regex: new RegExp(param.query.$search[item]) };
+      if (!isEmpty(param.query.$search[item])) {
+        query[item] = { $regex: new RegExp(param.query.$search[item]) };
+      }
     });
     context.params.query = { ...context.params.query, ...query };
   }
